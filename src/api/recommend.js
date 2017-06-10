0602e6a88b5d9e40d71b713481bf236a586be96d
chang <835702580@qq.com>
2017-06-11 01:11:46 +0800

    add getRecommend

---
+++ src/api/recommend.js
@@ -0,0 +1,15 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+
+  let result = jsonp(url, data, options)
+  return result
+}
