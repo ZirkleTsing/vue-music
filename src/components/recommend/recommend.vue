4ecbaa92979d7333eb6ff64a8a3577a0e288fe53
chang <835702580@qq.com>
2017-06-11 12:16:23 +0800

    add slider

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
-        <ul class="demo">
-          <li v-for="item in recommend">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </li>
-        </ul>
+      <div class="slider-wrapper" v-if="recommends.length">
+        <slider>
+          <div v-for="item in recommends">
+              <a :herf="item.linkUrl">
+                <img :src="item.picUrl">
+              </a>
+          </div>
+        </slider>
       </div>
-      <div class=recommend-list>
+      <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul></ul>
       </div>
@@ -21,11 +21,12 @@
 <script>
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import Slider from 'base/slider/slider'
 
   export default {
     data() {
       return {
-        recommend: []
+        recommends: []
       }
     },
     created() {
@@ -35,14 +36,38 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            this.recommend = res.data.slider
+            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
 
 <style lang="stylus">
-
+  @import "~common/stylus/variable"
+  
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
 </style>
