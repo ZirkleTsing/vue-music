bd9e4d70a0f7c3363f91f82a8abc7839853679ab
chang <835702580@qq.com>
2017-06-21 23:33:47 +0800

    recommend

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -4,7 +4,7 @@
       <div>
         <div class="slider-wrapper" v-if="recommends.length">
           <slider>
-            <div v-for="item in recommends">
+            <div v-for="item in recommends" >
                 <a :href="item.linkUrl">
                   <img @load="loadImage" :src="item.picUrl">
                 </a>
