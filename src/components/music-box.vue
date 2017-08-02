5f05983698dbf79969546f6f231cd9da0928805c
chang <835702580@qq.com>
2017-08-02 15:38:58 +0800

    music-box

--- src/components/music-box.vue
+++ src/components/music-box.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="musicbox-wrapper" v-show="list.length > 0">
-    <div class="normal-box" v-show="fullScreen">
-      <div class="mini-box-icon" @click="minibox">按钮</div>
-      <div class="title">{{songName}}</div>
-      <div class="subtitle">{{singerName}}</div>
+    <div class="normal-player" v-show="fullScreen">
+      <div class="top">
+        <div class="mini-icon" @click="minibox">
+          <i class="icon-back"></i>
+        </div>
+        <div class="title">{{songName}}</div>
+        <div class="subtitle">{{singerName}}</div>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+               <img class="image" :src="albumImg"> 
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-box" v-show="!fullScreen" @click="changeToNormalBox">
-      <div>{{songName}}</div>
-      <div>{{singerName}}</div>
+    <div class="mini-player" v-show="!fullScreen" @click="changeToNormalBox">
+      <div class="cd-wrapper">
+        <div class="cd">
+          <img class="image" :src="albumImg">
+        </div>
+      </div>
+      <div class="desc">
+        <div class="title">{{songName}}</div>
+        <div class="subtitle">{{singerName}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +38,8 @@
     data () {
       return {
         songName: '',
-        singerName: ''
+        singerName: '',
+        albumImg: ''
       }
     },
     computed: {
@@ -33,7 +54,9 @@
       'list' () {
         this.songName = this.list[this.currentIndex].name
         this.singerName = this.list[this.currentIndex].singer
-        console.table(this.list[this.currentIndex])
+        // console.table(this.list[this.currentIndex])
+        this.albumImg = this.list[this.currentIndex].image
+        console.log(this.albumImg)
       }
     },
     methods: {
@@ -54,7 +77,7 @@
 <style lang="stylus" scoped>
   @import "~common/stylus/variable"
   .musicbox-wrapper
-    .normal-box
+    .normal-player
       position: fixed
       top: 0
       left: 0
@@ -62,20 +85,103 @@
       height: 100%
       background-color: $color-background
       color: #FFFFFF
-      .mini-box-icon
-        position: absolute
-        top: 2px
-        left: 3px
-      .title
-        text-align: center
-      .subtitle
-        text-align: center
-    .mini-box
+      .top
+        position: relative
+        margin-bottom: 25px
+        .mini-icon
+          position: absolute
+          top: 0
+          left: 6px
+          z-index: 50
+          font-size: 0
+          .icon-back
+            font-size: 22px
+            padding: 9px
+            display: block
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          width: 70%
+          margin: 0 auto
+          text-align: center
+          overflow: hidden
+          line-height: 40px
+          font-size: 18px
+          white-space: nowrap
+          text-overflow: ellipsis
+          color: #FFFFFF
+        .subtitle
+          line-height: 20px
+          font-size: 14px
+          text-align: center
+          color: #FFFFFF
+      .middle
+        position: fixed
+        width: 100%
+        top: 80px
+        .middle-l
+          display: inline-block
+          vertical-align: top
+          position: relative
+          width: 100%
+          height: 0
+          padding-top: 80%
+          .cd-wrapper
+            position: absolute
+            top: 0
+            left: 10%
+            width: 80%
+            height: 100%
+            box-sizing: border-box
+            .cd
+              width: 100%
+              height: 100%
+              border-radius: 50%
+              .image
+                position: absolute
+                box-sizing: border-box
+                top: 0
+                left: 0
+                width: 100%
+                height: 100%
+                border-radius: 50%
+                border: 10px solid hsla(0,0%,100%,.1)
+    .mini-player
       position: fixed
+      display: block
       left: 0
       bottom: 0
+      z-index: 180
       height: 60px
       width: 100%
       color: #FFFFFF
-      background-color: #555
+      background-color: $color-highlight-background
+      // line-height: 60px
+      .cd-wrapper
+        position: relative
+        display: inline-block
+        width: 40px
+        height: 40px
+        padding: 10px 10px 0 20px
+        vertical-align: top
+        .cd
+          height: 100%
+          width: 100%
+          border-radius: 50%
+          .image
+            display: block
+            border-radius: 50%
+            width: 100%
+            height: 100%
+      .desc
+        display: inline-block
+        vertical-align: top
+        padding-top: 10px
+        .title
+          font-size: 14px
+          color: #FFFFFF
+          margin-bottom: 15px
+        .subtitle
+          font-size: 12px
+          color: hsla(0,0%,100%,.3)
 </style>
